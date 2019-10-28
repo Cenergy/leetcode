cc6f0d4009ae6a5178e42447752d7b005abdbf6c
Cenergy <673598118@qq.com>
2019-10-28 20:50:08 +0800

    add

---
+++ 1.两数之和.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=javascript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  for (i = 0; i < nums.length; i++) {
+    for (j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+// @lc code=end
+const a = twoSum([2, 7, 11, 15], 9);
+console.log("Rd: a", a);
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# leetcode
+# let us leetcode
